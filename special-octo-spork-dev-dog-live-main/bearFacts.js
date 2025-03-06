27afd894291056ab5aedb4de415a577d43d45930
CoreyBut <cwbutler1@una.edu>
2025-03-06 11:47:44 -0600

    Add files via upload

---
+++ special-octo-spork-dev-dog-live-main/bearFacts.js
@@ -0,0 +1,16 @@
+$(document).ready(function()){
+
+  console.log("Doc is Ready");
+});
+
+    let bearFacts = ["fact 1", "fact 2", "fact 3"];
+
+    console.log(bearFacts);
+
+    $('bearFactList').append('<li class="list-group-item">'
+    +bearFacts[0]+'</li>');
+
+    for(index = 0; )
+
+
+   
